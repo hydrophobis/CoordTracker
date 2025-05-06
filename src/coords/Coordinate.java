92dad7fe37b080567011f95f52cacb6074f96d83
hydrophobis <142858559+hydrophobis@users.noreply.github.com>
2025-05-06 09:58:30 -0500

    Fixed a wrong array length runtime error

--- src/coords/Coordinate.java
+++ src/coords/Coordinate.java
@@ -19,11 +19,16 @@ public class Coordinate {
     }
 
     public String toFileString() {
-        return name + ";" + x + ";" + y + ";" + z + ";" + description;
+        return name + ";" + x + ";" + y + ";" + z + ";" + (description.equals("") ? " " : description);
     }
 
     public static Coordinate fromFileString(String line) {
+
         String[] parts = line.split(";");
+        if(parts.length < 5){
+            System.out.println("Problem converting " + line + " to coordinate. Not enough pieces");
+            return null;
+        }
         return new Coordinate(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]),
                               Integer.parseInt(parts[3]), parts[4]);
     }
